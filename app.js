c823ff2e6a52a2ca8a777dbf5879b88524a32ce8
Karodar <karodar@mail.ru>
2018-09-24 19:38:51 +0300

    Refactoring code. Universal auto-analyze

--- app.js
+++ app.js
@@ -1,49 +1,64 @@
-window.filterUp = function (timeLimit, rateMax, ratioScore, delay) {
-    $('#header_balance').append($('<span></span>', { class: 'consoleLog' }));
+// Created by Karodar
+
+window.filterUp = function (timeLimit, rateMax, ratioScore) {
+    let delay = 3000,
+        info = $('<span></span>', {
+            class: 'consoleLog'
+        });
+    $('#header_balance').append(info);
 
     setInterval(function () {
-        var counter = 0;
-        function switchUp($line, squares) {
-            $line.removeAttr('style');
-            $line.css('background', '#C5E384');
+        let counter = 0;
 
-            squares.elems.forEach(function (priceItem) {
-                $(priceItem).removeAttr('style').css('background', '#9ACD32');
+        function switchUp($line, $square, event = false) {
+            $line.removeAttr('style');
+            $square.removeAttr('style');
+
+            $line.attr('sealed', 'line');
+            $square.attr('sealed', 'square');
+            if (event) $square.attr('sealed-event', true);
+            backlight();
+        }
+
+        function backlight() {
+            $('[sealed]').each(function (indx, dom) {
+                switch ($(dom).attr('sealed')) {
+                    case 'line':
+                        $(dom).css('background', '#C5E384');
+                        break;
+                    case 'square':
+                        $(dom).css('background', '#9ACD32');
+                        if ($(dom).attr('sealed-event'))
+                            $(dom).css('outline', '2px solid #B3446C');
+                        break;
+                }
             });
         }
 
-        $('[data-event-treeid]')
-            .filter(function (indx) {
-                var currentTime = $(this).find('.time-description > .green').text().split(':', 1)[0];
-                if (currentTime >= timeLimit)
-                    return true;
-                return false;
-            })
-            .each(function (indx, dom) {
-                var score, rates = { count: 0, elems: [] };
+        $('[data-event-treeid]').each(function (indx, dom) {
+            let timeGame = $(this).find('.time-description > .green').text().trim().split(':', 1)[0];
 
-                score = $(dom).find('.result-row .red').text().split(" ", 3)[2].split(':').sort(function (a, b) { return a - b; });
-                //console.log(score);
-                score = (typeof score == 'object') ? (score[0] / score[1] < ratioScore) : false;
+            if (timeGame >= timeLimit || !Boolean(timeGame)) {
+                let valScore = $(dom).find('.result-row .red').text().trim().split(" ", 1)[0].split(':').sort(function (a, b) { return a - b; });
+                valScore = valScore[0] / valScore[1];
 
+                if (valScore <= ratioScore) {
+                    $(this).find('.price').each(function (square_indx, elem) {
+                        $rate = $(elem).find('[data-selection-price]');
+                        if ($rate.data('selection-price') >= rateMax && $rate.data('selection-price') <= rateMax * 1.1) {
+                            counter++;
+                            switchUp($(dom), $(elem));
 
-                $(dom).find('.price').each(function (i, elem) {
-                    var currentRate = $(elem).find('[data-selection-price]').data('selection-price');
-                    if (typeof currentRate == 'number' && (currentRate > rateMax && currentRate < rateMax * 1.1)) {
-                        rates.count++;
-                        rates.elems[rates.count] = elem;
-                    }
-                });
-
-                //console.log('SCORE: ' + score + '\nRATES: ' + rates.count + '\n\n');
-
-                if (score && rates.count == 1) {
-                    counter++;
-
-                    switchUp($(dom), rates);
+                            $rate.parent().click(function (e) {
+                                switchUp($(dom), $(elem), true);
+                            });
+                        }
+                    });
                 }
-            });
-
-            $('.consoleLog').text('FIND! ' + counter + ' matches.');
+            }
+        });
+        
+        info.fadeTo(delay / 2, 1).text('FIND! ' + counter + ' matches');
+        info.fadeTo(delay / 2, 0.3);
     }, delay);
 };
